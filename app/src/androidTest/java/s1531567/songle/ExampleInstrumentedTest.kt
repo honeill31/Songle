1a7908d8a56f018eeed935971585dea36900da3b
Holly O'neill <s1531567@cagliari.inf.ed.ac.uk>
2017-09-30 17:52:25 +0100

    Initial commit

---
+++ app/src/androidTest/java/s1531567/songle/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package s1531567.songle
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("s1531567.songle", appContext.packageName)
+    }
+}
