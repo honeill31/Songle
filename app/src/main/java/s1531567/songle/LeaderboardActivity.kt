fc5da6d98a837ba747d7f3bfc1ff83dc82084ab4
honeill31 <honeill31@gmail.com>
2017-10-01 17:19:56 +0100

    Add empty leaderboard activity

---
+++ app/src/main/java/s1531567/songle/LeaderboardActivity.kt
@@ -0,0 +1,12 @@
+package s1531567.songle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaderboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaderboard)
+    }
+}
