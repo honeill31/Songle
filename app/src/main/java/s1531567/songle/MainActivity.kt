f56f7dce9c0c06977cb3a5c976764171bb6f73e3
honeill31 <honeill31@gmail.com>
2017-10-05 12:22:29 +0100

    Add test branch

--- app/src/main/java/s1531567/songle/MainActivity.kt
+++ app/src/main/java/s1531567/songle/MainActivity.kt
@@ -1,12 +1,34 @@
 package s1531567.songle
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    fun Context.toast(text: String){
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+
+    fun newIntent(context: Context): Intent {
+        val intent = Intent(this, LoginActivity::class.java)
+        return intent
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        butt.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                toast("Proceeding to Login Page")
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
